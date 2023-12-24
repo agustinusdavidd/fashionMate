13ba3d39e4a14da0623cded052981ec433d8cf14
BimaPL <bimapramudya26@gmail.com>
2023-12-24 11:35:42 +0700

    Feature/Calendar

--- app/src/main/java/com/example/presentation/screen/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/presentation/screen/calendar/CalendarScreen.kt
@@ -1,8 +1,102 @@
 package com.example.presentation.screen.calendar
 
+import android.annotation.SuppressLint
+import android.widget.CalendarView
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.NotificationsNone
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+import com.example.fashion_mate.R
+import com.example.presentation.component.TextHeader
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.unit.dp
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun CalendarScreen() {
-    
+fun CalendarScreen(navController : NavHostController) {
+    var date by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Calendar",
+                    style = MaterialTheme.typography.headlineLarge) },
+            )
+        },
+        content = {
+            Row(
+                modifier = Modifier
+                    .offset(6.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                AndroidView(factory = { CalendarView(it) }, update = {
+                    it.setOnDateChangeListener { CalendarView, year, month, day ->
+                        date = "$day - ${month + 1} - $year"
+                    }
+                })
+                Text(text = "Today",
+                    modifier = Modifier
+                        .wrapContentWidth(),
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 10.sp,
+                    color = Color(0xFF0A70C4),
+                    textAlign = TextAlign.Center)
+            }
+        }
+    )
 }
